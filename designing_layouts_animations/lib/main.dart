7ff92258591c2c50a6603bb62e4657586e54bf5c
Moussa Koné <moussa.kone@etu.univ-orleans.fr>
2025-04-19 10:24:36 +0200

    material application impl

--- designing_layouts_animations/lib/main.dart
+++ designing_layouts_animations/lib/main.dart
@@ -11,18 +11,43 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      theme: CupertinoThemeData(
-        primaryColor: CupertinoColors.activeBlue,
-        barBackgroundColor: CupertinoColors.activeBlue,
-        brightness: Brightness.light
+    //for ios Application
+    // return const CupertinoApp(
+    //   theme: CupertinoThemeData(
+    //     primaryColor: CupertinoColors.activeBlue,
+    //     barBackgroundColor: CupertinoColors.activeBlue,
+    //     brightness: Brightness.light
+    //   ),
+    //   home : CupertinoPageScaffold(
+    //       navigationBar: CupertinoNavigationBar(
+    //         middle: Text('hello World'),
+    //       ),
+    //       child: Center(child: Text('Hello World !!!'),))
+    // );
+
+    return  MaterialApp(
+      home: Scaffold(
+        appBar:  AppBar( // <-- corrigé ici
+          title: Text('MyApp'), // pas besoin de const ici non plus
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+          ],
+        ),
       ),
-      home : CupertinoPageScaffold(
-          navigationBar: CupertinoNavigationBar(
-            middle: Text('hello World'),
-          ),
-          child: Center(child: Text('Hello World !!!'),))
     );
+
 
   }
 }
